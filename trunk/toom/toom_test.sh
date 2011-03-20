9f57d8d873fe0638ea9454f3d4324efe88f0a18e
nuclearcat@gmail.com <nuclearcat@gmail.com@595b2e7c-6212-4f0b-031e-046fde5e6f50>
2011-03-20 14:35:45 +0000

    Initial commit

---
+++ trunk/toom/toom_test.sh
@@ -0,0 +1,68 @@
+#/bin/sh
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+./toom aaa 10 10
+sleep 2
+echo EOF
+./toom aaa 10 10
+sleep 20
+./toom aaa 10 10
+sleep 20
+
+
