e762988982a18d33d894fabdcc630228ff9781df
nuclearcat@gmail.com <nuclearcat@gmail.com@595b2e7c-6212-4f0b-031e-046fde5e6f50>
2011-11-21 11:26:55 +0000

    Updates for hash and new tool

--- iptop/iptop.c
+++ iptop/iptop.c
@@ -19,7 +19,7 @@
 #include <netinet/tcp.h>
 #include <string.h>
 #include <time.h>
-
+#include "uthash.h"
 
 #define ETHERTYPE_PPPOE_SESSION 0x8864
 #define PPPOE_SIZE              22                                                                                                                                                                        
@@ -27,16 +27,19 @@
 
 
 struct ippairs {
-    u_int32_t daddr;
+    u_int32_t addr;
     struct ippairs *next;
     int32_t refcount;
     uint32_t bytes;
     uint32_t packets;
+    UT_hash_handle hh;
 };
 
-struct ippairs *pairs;
+struct ippairs *pairs = NULL;
 int packets, packetstrigger;
 unsigned long long packetstotal,bytes;
+
+const int offset[2] = {offsetof(struct iphdr,daddr)/sizeof(u_int32_t), offsetof(struct iphdr,saddr)/sizeof(u_int32_t) };
 
 static int find_ip_eth(char* buf)
 {
@@ -69,60 +72,25 @@ static int find_ip_eth(char* buf)
 }
 
 void add_ip(struct iphdr *ip,unsigned int bytes,int dir) {
-    int iphash;// = hashlittle(&ip->daddr,4,(uint32_t)1) & HASHSIZE;
-    struct ippairs *ptr;// = &pairs[iphash];
-    u_int32_t ipaddr;
-    if (dir == 0) {
-	ipaddr = ip->daddr;
+//    int iphash;
+//    struct ippairs *ptr;
+    const u_int32_t ipaddr = ((u_int32_t*)ip)[offset[dir]];
+    struct ippairs *found = NULL;
+
+    HASH_FIND_INT( pairs, &ipaddr, found );
+
+    if (found) {
+	found->refcount++;
+	found->bytes+=bytes;
     } else {
-	ipaddr = ip->saddr;
+	found = malloc(sizeof(struct ippairs));
+	memset(found,0x0,sizeof(struct ippairs));
+	found->refcount++;
+	found->bytes+=bytes;
+	found->addr = ipaddr;
+	HASH_ADD_INT( pairs, addr, found );
     }
-    iphash = hashlittle(&ipaddr,4,(uint32_t)1) & HASHSIZE;
-    ptr = &pairs[iphash];
-
-//    printf("IP HASH %d PTR REFCOUNT %d\n",iphash,ptr->refcount);
-
-    if (ptr->refcount == 0) {
-//	printf("NEW\n");
-	ptr->refcount++;
-	ptr->bytes+=bytes;
-	memcpy(&ptr->daddr,&ipaddr,4);
-	return;
-    }
-//    abort();
-    while(ptr->refcount) {
-	if (memcmp(&ptr->daddr,&ipaddr,4)) {
-	    if (ptr->next != NULL)
-		ptr = ptr->next;
-	    else {
-//		printf("NEW COLL\n");
-		/* New, but collision */
-		ptr->next = malloc(sizeof(struct ippairs));
-		memset(ptr->next,0x0,sizeof(struct ippairs));
-		ptr = ptr->next;
-		ptr->refcount++;
-		ptr->bytes+=bytes;
-//		ptr->packets++;
-		memcpy(&ptr->daddr,&ipaddr,4);
-		return;
-	    }
-	} else {
-	    /* Existing */
-//		printf("EXIST\n");
-	    ptr->refcount++;
-	    ptr->bytes+=bytes;
-//	    ptr->packets++;
-
-//	    if ((double)(ptr->refcount/packetstrigger) == ((double)ptr->refcount/(double)packetstrigger) ) {
-//		struct in_addr s_addr;
-//		s_addr.s_addr = (in_addr_t)ptr->daddr;
-//		printf("Interesting %s %d\n",inet_ntoa(s_addr),ptr->refcount);
-//	    }
-
-
-	    return;
-	}
-    }
+    
 }
 
 
@@ -166,7 +134,7 @@ long timevaldiff(struct timeval *starttime, struct timeval *finishtime)
 }
 
 
-int compare (const void * a, const void * b)
+int compare_bytes (const void * a, const void * b)
 {
     struct ippairs *px, *py;
 
@@ -185,17 +153,14 @@ int main(int argc,char **argv)
     struct bpf_program fp;      /* hold compiled program     */
     bpf_u_int32 maskp;          /* subnet mask               */
     bpf_u_int32 netp;           /* ip                        */
-//    u_char* args = NULL;
     int args = 0;
-    int linktype,i,len;
-    struct ippairs *ptr;
-//    FILE *fff;
-//    char *buf;
+    int linktype,len;
     long timediff;
     struct timeval tv1, tv2;
     struct in_addr s_addr;
     char ipbuf[16];
     char *ntoaptr;
+    struct ippairs *found = NULL;
 
     /* Options must be passed in as a string because I am lazy */
     if(argc < 5){ 
@@ -209,18 +174,6 @@ int main(int argc,char **argv)
     if (!strcmp(argv[4],"src")) {
         args = 1;
     }
-
-//    printf("Allocating mem %d bytes for struct %d bytes\n",(sizeof(struct ippairs) * HASHSIZE),sizeof(struct ippairs));
-    pairs = malloc(sizeof(struct ippairs) * (HASHSIZE+1));
-    memset(pairs,0x0,sizeof(struct ippairs) * (HASHSIZE+1));
-
-    /* grab a device to peak into... */
-//    dev = pcap_lookupdev(errbuf);
-//    if(dev == NULL) { 
-//	printf("%s\n",errbuf); 
-//	exit(1); 
-//    }
-
 
     /* ask pcap for the network address and mask of the device */
     pcap_lookupnet(argv[1],&netp,&maskp,errbuf);
@@ -266,14 +219,12 @@ int main(int argc,char **argv)
     printf("Average packet size %llu (with ethernet header, max avg sz 1514)\n",bytes/packetstotal);
     printf("Time %ld, total bytes %lld, total speed %lld Kbit/s\n",timediff,bytes,bytes*8*1000/timediff/1024);
 
-    qsort (pairs, HASHSIZE, sizeof(struct ippairs), compare);
+    HASH_SORT(pairs, compare_bytes);
 
-    for (i=0;i<HASHSIZE;i++) {
-	 ptr = &pairs[i];
-	 while(ptr->refcount) {
+    for(found=pairs; found != NULL; found=found->hh.next) {
 	    memset(ipbuf,0x20,16);
 	    ipbuf[15] = 0x0;
-	    s_addr.s_addr = (in_addr_t)ptr->daddr;
+	    s_addr.s_addr = (in_addr_t)found->addr;
 	    ntoaptr = inet_ntoa(s_addr);
 	    if (!ntoaptr) {
 		perror("inet_ntoa()");
@@ -281,16 +232,8 @@ int main(int argc,char **argv)
 	    }
 	    len = strlen(ntoaptr);
 	    strncpy(ipbuf,ntoaptr,len);
-	    printf("%s %db %dp avg %db %llu%%b %llu%%p %llu Kbit/s\n",ipbuf,ptr->bytes,ptr->refcount,(ptr->bytes/ptr->refcount),ptr->bytes*100/bytes,ptr->refcount*100/packetstotal,((uint64_t)ptr->bytes*8/(uint64_t)timediff));
-	    if (!ptr->next) {
-		break;
-//		printf("N\n");
-    	    } else {
-		printf("Collision\n");
-	    }
-	    ptr = ptr->next;
-	 };
+	printf("%s %db %dp avg %db %llu%%b %llu%%p %llu Kbit/s\n",ipbuf,found->bytes,found->refcount,(found->bytes/found->refcount),found->bytes*100/bytes,found->refcount*100/packetstotal,((uint64_t)found->bytes*8/(uint64_t)timediff));
     }
-//    fprintf(stdout,"Program finished\n");
+
     return 0;
 }
